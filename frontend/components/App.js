b32072f1d3bd99f3799c7e5ef4a9be0719307712
Cody Denniston <dennistonc4817@gmail.com>
2022-05-28 19:19:06 -0500

    started

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,10 +1,51 @@
 import React from 'react'
 
 export default class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      todos: [
+        {
+          name: 'Organize Garage',
+          id: 1528817077286, // could look different, you could use a timestamp to generate it
+          completed: false
+        },
+        {
+          name: 'Bake Cookies',
+          id: 1528817084358,
+          completed: false
+        }
+      ]
+    }
+  }
+
+
+
   render() {
+    const {todos} = this.state
     return (
       <div>
-        Todo App
+        <h1>Todos</h1>
+
+
+        <ul>
+
+          {
+            todos.map(todo=>{
+              return (
+                <li>{todo.name} {todo.completed? <span>- completed</span> : <span></span>}</li>
+              )
+            })
+          }
+          
+        </ul>
+
+        <form>
+          <input/>
+          <button>Add</button>
+        </form>
+
+        <button>Clear</button>
       </div>
     )
   }
